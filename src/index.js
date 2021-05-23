809a96b0f42f3ea1d5a385bef80f4e0dd1716af4
kei0311github <78869994+kei0311github@users.noreply.github.com>
2021-05-23 23:16:29 +0900

    0522途中経過

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import "./styles.css";
+const onClickAdd = () => {
+  //インプットを取得
+  const inputData = document.getElementById("input-data").value;
+  document.getElementById("input-data").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  //li生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //p生成
+  const p = document.createElement("p");
+  p.innerText = inputData;
+
+  //完了ボタン生成
+  const finishButton = document.createElement("button");
+  finishButton.innerText = "完了";
+  finishButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  //組み合わせ
+  div.appendChild(p);
+  div.appendChild(finishButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  //未完了リストに追加
+  const ul = document.getElementById("incomplete-list");
+  ul.appendChild(li);
+};
+
+document.getElementById("add").addEventListener("click", () => onClickAdd());
